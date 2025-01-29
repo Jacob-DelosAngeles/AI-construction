6b0397b2267cadabfc1e8eb334fcdfeb05c01e4b
Francis Jacob Delos Angeles <dfrancisjacob65@gmail.com>
2025-01-29 16:46:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ from predict_page import show_predict_page
 # st.title('🏗️ Velocity AI')
 # st.info('Forward Progress. Proper Direction')
 
+st.sidebar.selectbox("Explore or Predict", ("Predict, Explore"))
 show_predict_page()
