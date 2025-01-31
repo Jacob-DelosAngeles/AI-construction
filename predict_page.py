ba7adb57cfb4bead415610e8914b54037e3d8e87
Francis Jacob Delos Angeles <dfrancisjacob65@gmail.com>
2025-01-31 20:41:48 +0800

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -13,6 +13,20 @@ regressor = data['model']
 def show_predict_page():
   st.title(" 🏛️StrucSure: Concrete's Compressive Strength Intelligent Predictor")
   st.info("""#### Please input information """)
+
+# Variables for prediction
+
+cement = st.number_input("Cement (g)", placeholder = "Type a number")
+slag = st.number_input("Slag (g)", placeholder = "Type a number") 
+flyash = st.number_input("Flyash (g)", placeholder = "Type a number")
+water = st.number_input("Water (g)", placeholder = "Type a number")
+superplasticizer = st.number_input("Superplasticizer (g)", placeholder = "Type a number")
+courseAggregate = st.number_input("Course Aggregate (g)", placeholder = "Type a number")
+fineAggregate = st.number_input("Fine Aggregate (g)", placeholder = "Type a number")
+waterToCement = st.number_input("Water to Cement Ratio (g)", placeholder = "Type a number")
+
+predict = st.button("Predict Strength")
+
   
   
 
