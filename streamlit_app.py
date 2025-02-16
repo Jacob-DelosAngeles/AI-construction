032f400ce4a454b745b15e54d20b81113f3dcdbc
Francis Jacob Delos Angeles <dfrancisjacob65@gmail.com>
2025-02-16 17:57:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,21 @@ from predict_page import show_predict_page
 from explore_page import show_explore_page
 
 
-page = st.sidebar.selectbox("Explore or Predict", ("Predict", "Explore"))
+sidebar = st.sidebar #.selectbox("Explore or Predict", ("Predict", "Explore"))
+explore = sidebar.button("Explore")
+predict = sidebar.button("Predict")
 
-# show_explore_page()
-if page == "Explore":
+if explore:
   show_explore_page()
-else:
+
+if predict:
   show_predict_page()
+
+else:
+  show_explore_page()
+  
+# show_explore_page()
+#if page == "Explore":
+  #show_explore_page()
+#else:
+  #show_predict_page()
