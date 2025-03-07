ada4d45cf6ace87dbec0e41af83ce1e4a04df99f
Francis Jacob Delos Angeles <dfrancisjacob65@gmail.com>
2025-03-07 09:00:22 +0800

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -25,4 +25,4 @@ def show_explore_page():
   st.title("🤖 VelocityAI 🏗️")
 
   st.info("""### Building Strength with Data-Driven Intelligence """)
-  st.info("""#### At Velocity AI, we are redefining how the construction industry ensures concrete strength. Our AI-powered tool, StrucSure, predicts the compressive strength of concrete with high accuracy, helping engineers, developers, and construction firms save time, reduce material waste, and improve quality control. Instead of waiting for lab tests, you can get instant predictions using our machine learning model—helping you make faster, smarter, and cost-efficient decisions in your projects.""")
+  st.write("""#### At Velocity AI, we are redefining how the construction industry ensures concrete strength. Our AI-powered tool, StrucSure, predicts the compressive strength of concrete with high accuracy, helping engineers, developers, and construction firms save time, reduce material waste, and improve quality control. Instead of waiting for lab tests, you can get instant predictions using our machine learning model—helping you make faster, smarter, and cost-efficient decisions in your projects.""")
