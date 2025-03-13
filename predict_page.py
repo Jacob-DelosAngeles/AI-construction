1fef1acb1b701ce126702fa1524c08aef4b31d93
Francis Jacob Delos Angeles <dfrancisjacob65@gmail.com>
2025-03-13 13:23:12 +0800

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -86,7 +86,7 @@ def show_predict_page():
 # Variables for prediction
 
   st.subheader('Primary Materials')
-  cement = st.number_input("Cement (g)", min_value = 102, max_value = 540, step = 1)
+  cement = st.number_input("Cement (g)", min_value = 102, max_value = 540, step = 1, help="Enter a value between 100 and 600 grams")
   courseAggregate = st.number_input("Course Aggregate (g)", placeholder = "Type a number")
   fineAggregate = st.number_input("Fine Aggregate (g)", placeholder = "Type a number")
   water = st.number_input("Water (g)", placeholder = "Type a number")
