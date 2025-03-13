1713fc6356cb7de341221c09d6348e7869c9b276
Francis Jacob Delos Angeles <dfrancisjacob65@gmail.com>
2025-03-13 13:38:46 +0800

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -89,7 +89,7 @@ def show_predict_page():
   cement = st.number_input("Cement (g)", min_value = 102.0, max_value = 540.0, step = 1.0, value=None, help="Enter a value between 102 and 540 grams")
   courseAggregate = st.number_input("Course Aggregate (g)", min_value = 801.0, max_value = 1145.0, step = 1.0, value=None, help="Enter a value between 801 and 1145 grams")
   fineAggregate = st.number_input("Fine Aggregate (g)", min_value = 549.0, max_value = 992.6, step = 1.0, value=None, help="Enter a value between 549 and 992.6 grams")
-  water = st.number_input("Water (g)", min_value = 121.8, max_value = 247, step = 1.0, value=None, help="Enter a value between 121.8 and 247 grams")
+  water = st.number_input("Water (g)", min_value = 121.8, max_value = 247.0, step = 1.0, value=None, help="Enter a value between 121.8 and 247 grams")
 
   st.subheader('Admixtures')
   superplasticizer = st.number_input("Superplasticizer (g)", min_value = 0.0, max_value = 32.2, step = 1.0, value=None, help="Enter a value between 0 and 32.2 grams")
