a44045dd6d50a19580d0604bb19b0f15668c592b
Francis Jacob Delos Angeles <dfrancisjacob65@gmail.com>
2025-03-24 11:39:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,23 @@
 import streamlit as st
 from predict_page import show_predict_page
-from explore_page import show_explore_page
+from overview_page import show_overview_page
 
 
 
 # Initialize session state
 if "page" not in st.session_state:
-  st.session_state.page = "Explore"  # Default page
+  st.session_state.page = "Overview"  # Default page
 
 # Sidebar buttons
 st.sidebar.title("🔹Main Menu")
-if st.sidebar.button("🌍 Explore  "):
-  st.session_state.page = "Explore"
+if st.sidebar.button("🌍 Overview  "):
+  st.session_state.page = "Overview"
 
 if st.sidebar.button("✅ Predict  "):
   st.session_state.page = "Predict"
 
 # Display the selected page
-if st.session_state.page == "Explore":
-  show_explore_page()
+if st.session_state.page == "Overview":
+  show_overview_page()
 elif st.session_state.page == "Predict":
   show_predict_page()
