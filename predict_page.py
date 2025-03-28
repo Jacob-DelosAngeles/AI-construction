7057c0411fcc262621ec952ad2483e5d01467faf
Francis Jacob Delos Angeles <dfrancisjacob65@gmail.com>
2025-03-28 15:43:35 +0800

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -149,7 +149,7 @@ def show_predict_page():
     X_norm = (X - data['mu'])/data['sigma']
   
     strength = model.predict(X_norm)    # model- neural network, regressor - linear regression
-    st.subheader(f"Estimated Strength: {strength})    # [0][0] - NN, [0] - linear regression
+    st.subheader(f"Estimated Strength: {float(strength[0]):.2f} csMPa")  
     
 
     
