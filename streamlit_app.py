79b61bce74c88706f7954212469fecdd8c8ed0c3
Francis Jacob Delos Angeles <dfrancisjacob65@gmail.com>
2025-04-04 22:55:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from predict_page import show_predict_page
 from overview_page import show_overview_page
 #from st_pages import Page, Section, show_pages, add_page_title, hide_pages
 
